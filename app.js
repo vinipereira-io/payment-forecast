f8e179d48aae8c5d7f1b4ea2bcee4221f4623068
Vini Pereira <vini@saltandlime.com.au>
2023-04-17 14:48:23 +1000

    refactor function to concentrate all the other functions and apply an adjustment to monthly rate based on an weighted average

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 function bubble_repaymentForecast(properties, context) {
-    let dailyRate = properties.rate / 365;
-	let repayments = [];
-	let prevBalance = Math.abs(properties.loanAmount);
-    let prevDate = properties.startingDate;
 
     function roundNumber(number) {
         const roundedNumber = Math.round(number * 100) / 100;
 
         return roundedNumber;
     }
-    
+
     function setNewDate(date, offset, frequency) {
         const referenceDate = new Date(date);
 
@@ -29,67 +25,139 @@ function bubble_repaymentForecast(properties, context) {
             const offsetInDays = offset * 7;
             return new Date(referenceDate.getFullYear(), referenceDate.getMonth(), referenceDate.getDate() + offsetInDays)
         } else {
-            return 'error: frequency not defined'
+            console.log('error: frequency not defined');
         }
     }
 
-    for (let i = 1; i <= properties.numberPayments; i++) {
-		const newDate = setNewDate(properties.startingDate, i, properties.frequencyPayments);
-        const diffTime = Math.abs(newDate - prevDate);
-        const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24));
-        const newInterest = roundNumber(diffDays * dailyRate * prevBalance);
-        const newPrincipal = roundNumber(Math.abs(properties.repaymentAmount) - newInterest);
-
-        if (i === properties.numberPayments) {
-            const lastRepaymentAmount = roundNumber(prevBalance + newInterest);
-            const lastPrincipal = roundNumber(lastRepaymentAmount - newInterest);
-		
-            const obj = {
-                'id': i,
-                'date': new Date(newDate).toLocaleDateString('en-US'),
-                'openingBalance': prevBalance,
-                'repayment': lastRepaymentAmount,
-                'interest': newInterest,
-                'principal': lastPrincipal,
-                'closingBalance': roundNumber(prevBalance - lastPrincipal)
-            }  
-
-            repayments.push(obj);
+    function convertRateToFrequency(rate, frequency) {
+        if (frequency === 'monthly') {
+            return rate / 12;
+        } else if (frequency === 'fortnightly') {
+            return rate / 365 * 14;
+        } else if (frequency === 'weekly') {
+            return rate / 365 * 7;
         } else {
-            const obj = {
-                'id': i,
-                'date': new Date(newDate).toLocaleDateString('en-US'),
-                'openingBalance': prevBalance,
-                'repayment': Math.abs(properties.repaymentAmount),
-                'interest': newInterest,
-                'principal': newPrincipal,
-                'closingBalance': roundNumber(prevBalance - newPrincipal)
-            }
+            console.log('error: frequency not defined');
+        }
+    }
 
-            repayments.push(obj);
+    function calculatePayment(rate, numberPayments, loanAmount) {
+        const Finance = require('financejs');
+        const finance = new Finance();
+
+        const payment = finance.PMT(rate, numberPayments, -Math.abs(loanAmount));
+        const roundedPayment = Math.ceil(payment * 100) / 100;
+        
+        return roundedPayment
+    }
+
+    function setRepaymentForecast(loanAmount, rate, payment, numberPayments, frequencyPayments, startingDate) {
+        let dailyRate = rate / 365;
+        let repayments = [];
+        let prevBalance = Math.abs(loanAmount);
+        let prevDate = startingDate;
+    
+        for (let i = 1; i <= numberPayments; i++) {
+            const newDate = setNewDate(startingDate, i, frequencyPayments);
+            const diffTime = Math.abs(newDate - prevDate);
+            const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24));
+            const newInterest = roundNumber(diffDays * dailyRate * prevBalance);
+            const newPrincipal = roundNumber(Math.abs(payment) - newInterest);
+    
+            if (i === numberPayments) {
+                const lastRepayment = roundNumber(prevBalance + newInterest);
+                const lastPrincipal = roundNumber(lastRepayment - newInterest);
+            
+                const obj = {
+                    'id': i,
+                    'date': new Date(newDate).toLocaleDateString('en-US'),
+                    'openingBalance': prevBalance,
+                    'repayment': lastRepayment,
+                    'interest': newInterest,
+                    'principal': lastPrincipal,
+                    'closingBalance': roundNumber(prevBalance - lastPrincipal)
+                }  
+    
+                repayments.push(obj);
+            } else {
+                const obj = {
+                    'id': i,
+                    'date': new Date(newDate).toLocaleDateString('en-US'),
+                    'openingBalance': prevBalance,
+                    'repayment': Math.abs(payment),
+                    'interest': newInterest,
+                    'principal': newPrincipal,
+                    'closingBalance': roundNumber(prevBalance - newPrincipal)
+                }
+    
+                repayments.push(obj);
+            }
+    
+            prevBalance = roundNumber(prevBalance - newPrincipal);
+            prevDate = newDate;
         }
 
-        prevBalance = roundNumber(prevBalance - newPrincipal);
-        prevDate = newDate;
+        return repayments;
     }
+
+    function adjustMonthlyPayment(originalForecast) {
     
+        let lastDate = properties.startingDate;
+        
+        let sumOfProducts = 0;
+        
+        for (entry of originalForecast) {
+            const diffTime = new Date(entry.date) - lastDate;
+            const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24));
+        
+            sumOfProducts += diffDays * entry.interest;
+        
+            lastDate = new Date(entry.date);
+        }
+        
+        let sumOfInterests = 0;
+        
+        for (entry of originalForecast) {
+            sumOfInterests += entry.interest;
+        }
+        
+        const daysPerMonth = sumOfProducts / sumOfInterests;
+
+        const adjustedMonthlyRate = properties.rate / 365 * daysPerMonth;
+
+        const adjustedPayment = calculatePayment(adjustedMonthlyRate, properties.numberPayments, properties.loanAmount)
+
+        return adjustedPayment;        
+    }
+
+const convertedRate = convertRateToFrequency(properties.rate, properties.frequencyPayments);
+
+let payment = calculatePayment(convertedRate, properties.numberPayments, properties.loanAmount);
+
+let repaymentForecast = setRepaymentForecast(properties.loanAmount, properties.rate, payment, properties.numberPayments, properties.frequencyPayments, properties.startingDate);
+
+if (properties.frequencyPayments === 'monthly') {
+    payment = adjustMonthlyPayment(repaymentForecast);
+
+    repaymentForecast = setRepaymentForecast(properties.loanAmount, properties.rate, payment, properties.numberPayments, properties.frequencyPayments, properties.startingDate);
+}
+
 //return the JSON object as a string, because it will be returned as string on Bubble,
 //and parsed in the app (using a generic API call)
 return {
-    returnRepayments: JSON.stringify(repayments)
+    returnRepaymentForecast: JSON.stringify(repaymentForecast)
 }
 }
 
 console.log(bubble_repaymentForecast(
-    {
-        rate: 0.35,
-        numberPayments: 156,
-        frequencyPayments: 'fortnightly',
-        loanAmount: 20000,
-        repaymentAmount: 306.82,
-        startingDate: 1680181200000
-    },
-    {   
+        {
+            rate: 0.35,
+            numberPayments: 12,
+            frequencyPayments: 'monthly',
+            loanAmount: 20000,
+            startingDate: 1681308000000
+        },
+        {   
 
-    }
-));
+        }
+    ))
